3a9ab52f3cdab207a7c3c26473c9086f05d423ba
Danai24 <122793694+Danai24@users.noreply.github.com>
2023-03-13 09:20:47 +0100

    Add files via upload

---
+++ javascript-del2.js
@@ -0,0 +1,68 @@
+//opg2
+// var tekst = "Hej verden";
+// alert(tekst); 
+
+// //opg 3
+// var start = "Hej";
+//     var slut = "verden";
+//     var samlet = start + slut;
+
+//opg4
+//     var tal1 = 2;
+//     var tal2 = 8;
+//     var tallet = tal1 + tal2; 
+
+// document.write(tallet);
+
+//opg5
+// var tekst = "det er en teskt";
+// var tal = 1337;
+// var plus = tekst + tal;
+
+// document.write(plus)
+
+//opg6
+// var tal1 = 25;
+// var tal2 = 2;
+// var last = tal1 * tal2;
+
+// alert(last)
+
+//opg7 vi kan skrive tal i den når vi brug prompt().
+// var tal = prompt("skriv et tal");
+
+// alert(tal)
+
+//opg8
+// var tal = prompt("skriv tal")
+
+// if(isNaN(variabel)){
+//     alert(" er IKKE et tal");
+// }
+// else{
+//     alert(" er et tal");
+// }
+ 
+//opg10
+// var tal = prompt("Skriv et tal");
+// while(isNaN(tal)){
+//  tal = prompt("Skriv et tal");
+// }
+
+//opg11 
+// document.write(typeof 25)
+// document.write(typeof 'Danai')
+// document.write(typeof 5 === 'number')
+
+
+//opg12
+// let name = prompt("skriv den navn");
+
+// if(name){
+//     alert('true ' + name)
+// }
+// else{
+//     alert('false' + name)
+// }
+
+//opg13 
